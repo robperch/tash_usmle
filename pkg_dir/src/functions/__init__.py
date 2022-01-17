6f07fd9ff181022ac2a930cba1030097531b7344
Roberto919 <robertosysm@gmail.com>
2022-01-16 21:52:35 -0600

    adding func to create tuples with video's data and converting to df

--- pkg_dir/src/functions/__init__.py
+++ pkg_dir/src/functions/__init__.py
@@ -2,6 +2,12 @@
 
 
 ## Imports
+from .usmle_videos_funcs import (
+
+    videos_entries_list,
+    video_data_to_df,
+
+)
 
 
 
--- pkg_dir/src/functions/usmle_videos_funcs.py
+++ pkg_dir/src/functions/usmle_videos_funcs.py
@@ -10,7 +10,7 @@
 
 
 "--- Standard library imports ---"
-
+import os
 
 "--- Third party imports ---"
 from moviepy.editor import VideoFileClip
@@ -29,13 +29,52 @@ from pkg_dir.config import *
 "----------------------------------------------------------------------------------------------------------------------"
 
 
-## Reading through all the video files and store names and durations in a nested dictionary
-def videos_nested_dictionary():
+## Reading through all the video files and store names and durations in a list
+def videos_entries_list():
     """
-    Reading through all the video files and store names and durations in a nested dictionary
+    Reading through all the video files and store names and durations in a list
 
-    :return videos_dict: (dictionary) nested dictionary containing topics, subtopics, videos and durations
+    :return videos_entries: (list) list of tuples containing topics, subtopics, videos and durations
     """
+
+
+    ## Dictionary comprehension to build nested dict
+    videos_entries = [
+
+        (
+            topic,
+            subtopic,
+            video,
+            VideoFileClip(usmle_2020_videos_path + "/" + topic + "/" + subtopic + "/" + video).duration/60
+        )
+
+        for topic in os.listdir(usmle_2020_videos_path) if ".DS_Store" not in topic
+        for subtopic in os.listdir(usmle_2020_videos_path + "/" + topic) if ".DS_Store" not in subtopic
+        for video in os.listdir(usmle_2020_videos_path + "/" + topic + "/" + subtopic) if ".mp4" in video
+
+    ]
+
+
+    return videos_entries
+
+
+
+## Creating a dataframe from the video data stored as tuples
+def video_data_to_df(videos_entries, save_csv):
+    """
+    Creating a dataframe from the video data stored as tuples
+
+    :param videos_entries: (list) list of tuples containing topics, subtopics, videos and durations
+    :param save_csv: (boolean) condition to specify if the df is saved locally as a csv file
+    :return dfx: (dataframe) df with all data arranged in dataframe
+    """
+
+
+    ## Generating dataframe from tuples
+    dfx = pd.DataFrame(videos_entries)
+
+
+    return dfx
 
 
 
